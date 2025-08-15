bf2d4768f62eb7d90b0172ea40c6932fd9b0100b
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-08-16 03:05:06 +0530

    changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -25,26 +25,29 @@ opt.listchars = {
   -- eol = "¬",
 }
 
--- vim.opt.guicursor = {
---   'n-v-c:block-blinkwait700-blinkoff400-blinkon250',   -- Normal, Visual, Command-line mode with blinking block cursor
---   'i-ci-ve:block-blinkwait700-blinkoff400-blinkon250', -- Insert, Replace mode with blinking vertical bar cursor
---   'r-cr:block-blinkwait700-blinkoff400-blinkon250',    -- Replace mode with blinking horizontal cursor
+-- opt.guicursor = {
+--   'n-v-c:block-blinkwait700-blinkoff400-blinkon250',     -- Normal, Visual, Command-line: blinking block
+--   'i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250',   -- Insert modes: blinking vertical bar (25% width)
+--   'r-cr:hor20-blinkwait700-blinkoff400-blinkon250',      -- Replace modes: blinking horizontal bar (20% height)
 -- }
 
-vim.opt['guicursor'] = ""
+-- opt['guicursor'] = ""
+
+opt.showmatch = true
+opt.matchtime = 3
 
 -- toggle statusline
--- vim.opt.laststatus = 2 -- 0 and 2
-vim.opt.ruler = false
+-- opt.laststatus = 2 -- 0 and 2
+opt.ruler = false
 
 -- toggle show tabs on top
-vim.opt.showtabline = 0
+opt.showtabline = 0
 
 -- toggle show modes in command line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- to make the lsp suggestion transparent
--- vim.opt.pumblend = 30
+-- opt.pumblend = 30
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
@@ -98,48 +101,43 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
 -- Make line numbers default
-vim.opt.number = true
-vim.opt.rnu = true
+opt.number = true
+opt.rnu = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
-vim.opt.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Decrease update time
-vim.opt.updatetime = 250
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Minimal number of screen lines to keep above and below the cursor.
--- vim.opt.scrolloff = 15
+-- opt.scrolloff = 15
 
